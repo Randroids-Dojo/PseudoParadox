420d4331cf43633087437b3431599061614e93ff
Randy Lutcavich <Rlutcavich@gmail.com>
2021-09-23 12:01:20 -0700

    [WIP] Starting a high level integration test

---
+++ Assets/_PseudoParadox/Tests/TimeLoopTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TimeLoopTest
+    {
+
+
+    [UnityTest]
+    public IEnumerator AsteroidsMoveDown()
+    {
+        // Given player moved along path [p] during time range [t1] to [t2]
+        // When player time travels
+        // Then variant player moves along path [p] during time range [t1] to [t2]
+
+        yield break;
+
+    }
+}
